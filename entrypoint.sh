ff0acf27129e470ef3ea908f1c8fe8c526212236
tristan straub <tristan@straub.id.au>
2020-02-01 09:39:12 +1100

    Strict entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
+set -euxo pipefail
+
 pwd
 ls /github/workspace
 ls /github/home
 ls /github/workflow
+
+cat /github/home/event.json
 
 if [ -z "$@" ]; then
     clojure -A:test
